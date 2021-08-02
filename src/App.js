1e4fd2289087d5a69ec17c5619af34737c6a9d7e
Emmanx <aneriemmax@gmail.com>
2021-08-02 12:44:25 +0100

    feat: 🎸 Initial CRA setup with webengage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+  React.useEffect(() => {
+    console.log('WEB ENGAGE SETUP')
+    
+    window.webengage.onReady(function () {
+      window.webengage.webpush.prompt()
+    })
+
+    window.webengage.onReady(function () {
+      window.webengage.webpush.onSubscribe(function () {
+        alert('Thanks! You are now subscribed')
+      })
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
